08877d07390582422438ea8a10cf429e188b3470
svetozar12 <s.gospodinov02@gmail.com>
2023-09-13 00:22:36 +0300

    update node

--- backend/with-nodejs/api/src/config/swagger.ts
+++ backend/with-nodejs/api/src/config/swagger.ts
@@ -1,4 +1,4 @@
-const swaggerJsdoc = require('swagger-jsdoc');
+import swaggerJsdoc from 'swagger-jsdoc';
 
 const options = {
   swaggerDefinition: {
--- backend/with-nodejs/api/src/main.ts
+++ backend/with-nodejs/api/src/main.ts
@@ -3,19 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import express from "express";
-import * as path from "path";
-const swaggerUi = require("swagger-ui-express");
-import { exampleRouter } from "./routes/example/example";
-import { specs } from "./config/swagger";
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { exampleRouter } from './routes/example/example';
+import { specs } from './config/swagger';
 
 const app = express();
 
-app.use("/api", exampleRouter);
-app.use("/v1/swagger/doc.json", swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api', exampleRouter);
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/v1/swagger/doc.json', (req, res) => res.send(specs));
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on("error", console.error);
+server.on('error', console.error);
