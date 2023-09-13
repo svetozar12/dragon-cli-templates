7f435b220074ed92e01c7c09d18b04b28fac6e3b
svetozar12 <s.gospodinov02@gmail.com>
2023-09-13 21:25:25 +0300

    update nodejs template

--- backend/with-nodejs/api/src/config/swagger.ts
+++ backend/with-nodejs/api/src/config/swagger.ts
@@ -1,15 +1,15 @@
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJsdoc from "swagger-jsdoc";
 
 const options = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Express Swagger API',
-      version: '1.0.0',
-      description: 'API documentation using Swagger',
+      title: "Express Swagger API",
+      version: "1.0.0",
+      description: "API documentation using Swagger",
     },
   },
-  apis: ['./apps/api/src/routes/**/*.ts'], // Path to the API routes containing JSDoc comments
+  apis: ["./apps/api/src/routes/**/*.ts", "./*.js"], // Path to the API routes containing JSDoc comments
 };
 
 export const specs = swaggerJsdoc(options);
